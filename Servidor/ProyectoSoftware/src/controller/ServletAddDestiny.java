e2209650dc58fed81dacd9dacc12fbc581f542b0
samugascon <samuel_95_alc@hotmail.com>
2016-04-15 11:06:50 +0200

    acentos

--- Servidor/ProyectoSoftware/src/controller/ServletAddDestiny.java
+++ Servidor/ProyectoSoftware/src/controller/ServletAddDestiny.java
@@ -30,7 +30,7 @@ public class ServletAddDestiny extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		 request.setCharacterEncoding("UTF-8");
 		//try {
 			//Connection conecta = (Connection) AccesoBase.getDBConnection();
 			String pais=request.getParameter("countryName");
